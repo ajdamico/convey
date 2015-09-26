b055a241431a5091277095b6984ea3be84e96f60
Anthony Damico <ajdamico@gmail.com>
2015-09-26 14:35:07 -0400

    w vcov

--- R/all_funs.R
+++ R/all_funs.R
@@ -489,12 +489,18 @@ coef.cvystat <- function(object, ...) {
 # cvystat SE method
 #' @export
 SE.cvystat <- function(object, ...) {
-    v <- survey:::vcov.svystat(object)
+    v <- vcov(object)
     if (!is.matrix(v) || NCOL(v) == 1) 
         sqrt(v)
     else sqrt(diag(v))
 }
 
+# cvystat vcov method
+#' @export
+vcov.cvystat <- function (object, ...) 
+{
+    as.matrix(attr(object, "var"))
+}
 
 
 #' prepare linearized svydesign objects for convey package
