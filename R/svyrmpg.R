175c9757d0bd8bad70167f867f790c55557a8412
Djalma Pessoa <pessoad@gmail.com>
2015-10-10 15:47:33 -0300

    fix svyrmpg.svrep

--- R/svyrmpg.R
+++ R/svyrmpg.R
@@ -100,7 +100,7 @@ svyrmpg.survey.design <- function(formula, design, order = 0.5, percent = 0.6, c
 #' @export
 svyrmpg.svyrep.design <- function(formula, design, order = 0.5, percent = 0.6,na.rm=FALSE, ...) {
 
-	convey_prep_needs_to_be_run <- ( "svyrep.design" %in% class( design ) & "survey.design" %in% class( attr( design , "full_design" ) ) ) | is.null(attr(design, "full_design"))
+  convey_prep_needs_to_be_run <- ( "svyrep.design" %in% class( design ) & "survey.design" %in% class( attr( design , "full_design" ) ) ) | is.null(attr(design, "full_design"))
 
   if (convey_prep_needs_to_be_run)
     stop("you must run the ?convey_prep function on your linearized survey design object immediately after creating it with the svrepdesign() or as.svrepdesign() functions.")
@@ -112,26 +112,35 @@ svyrmpg.svyrep.design <- function(formula, design, order = 0.5, percent = 0.6,na
   if ("logical" %in% class(attr(design, "full_design")))
     full_design <- design else full_design <- attr(design, "full_design")
 
-	
+
     inc <- terms.formula(formula)[[2]]
     df <- model.frame(design)
     incvar <- df[[as.character(inc)]]
-    ComputeRmpg <- function(x, w, order, percent) {
-        tresh <- percent * computeQuantiles(incvar, w, p = order)
-        arpr <- sum((incvar <= tresh) * w)/sum(w)
-        indpoor <- (x <= tresh)
-        medp <- computeQuantiles(x[indpoor], w[indpoor], p = 0.5)
-        1 - (medp/tresh)
+    wsf<- weights(full_design,"sampling")
+    df_full<- model.frame(full_design)
+    incvec <-  df_full[[as.character(inc)]]
+    names(incvec)<-names(wsf)<- row.names(df_full)
+    ind<- row.names(df)
+    ComputeRmpg <- function(xf, wf, ind, order, percent) {
+      tresh <- percent * convey:::computeQuantiles(xf, wf, p = order)
+      x<-xf[ind]
+      w<- wf[ind]
+      indpoor <- (x <= tresh)
+      medp <- convey:::computeQuantiles(x[indpoor], w[indpoor], p = 0.5)
+      1 - (medp/tresh)
     }
     ws <- weights(design, "sampling")
-    rval <- ComputeRmpg(incvar, ws, order = order, percent = percent)
-    ww <- weights(design, "analysis")
-    qq <- apply(ww, 2, function(wi) ComputeRmpg(incvar, wi, order = order, percent = percent))
+    rval <- ComputeRmpg(xf = incvec, wf=wsf, ind= ind, order = order, percent = percent)
+    wwf <- weights(full_design, "analysis")
+    qq <- apply(wwf, 2, function(wi){
+      names(wi)<- row.names(df_full)
+      ComputeRmpg(incvec, wi, ind=ind, order = order,percent = percent)
+    })
     variance <- svrVar(qq, design$scale, design$rscales, mse = design$mse, coef = rval)
 
-	variance <- as.matrix( variance )
+    variance <- as.matrix( variance )
 
-	rownames( variance ) <- names( rval ) <- strsplit( as.character( formula )[[2]] , ' \\+ ' )[[1]]
+    rownames( variance ) <- names( rval ) <- strsplit( as.character( formula )[[2]] , ' \\+ ' )[[1]]
     class(rval) <- "cvystat"
     attr(rval, "var") <- variance
     attr(rval, "statistic") <- "rmpg"
