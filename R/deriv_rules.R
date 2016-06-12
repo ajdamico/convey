7cb55c3c69dd220e8f63be2c0220506d2f67763d
Anthony Damico <ajdamico@gmail.com>
2016-06-12 09:59:22 -0400

    move contrastinf to its own file

--- R/deriv_rules.R
+++ R/deriv_rules.R
@@ -88,23 +88,6 @@ fun_par_inf <- function(big_s, F, Fprime, ...) {
 # functions.
 
 
-#' @rdname funs
-#' @export
-contrastinf <- function(exprlist, infunlist) {
-    datalist <- lapply(infunlist, function(thresh) thresh$value)
-    listlin <- lapply(infunlist, function(thresh) thresh$lin)
-    if (!is.list(exprlist))
-        exprlist <- list(contrast = exprlist)
-    dexprlist <- lapply(exprlist, function(expr) deriv(expr, names(datalist))[[1]])
-    value <- eval(exprlist$contrast, datalist)
-    values_deriv <- lapply(dexprlist, function(dexpr) eval(do.call(substitute, list(dexpr,
-        datalist))))
-    matval <- attr(values_deriv$contrast, "gradient")
-    matlin <- matrix(NA, length(infunlist[[1]]$lin), ncol(matval))
-    for (i in 1:length(listlin)) matlin[, i] <- listlin[[i]]
-    IT_lin <- matlin %*% t(matval)
-    list(value = value, lin = IT_lin)
-}
 
 
 
