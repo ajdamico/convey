54961b98c6ab82a558f43490744acd1c37e6057d
Djalma Pessoa <pessoad@gmail.com>
2016-06-12 16:50:35 -0300

    contrastinf documentation

--- R/contrastinf.R
+++ R/contrastinf.R
@@ -1,25 +1,48 @@
-#' title here
+#' Computes the linearized variable of a function of a survey statististics
+
 #'
-#' subtitle here
+#' @param exprlist a call
+#' @param infunlist a list of list, each having two components: value - the estimate and lin - the linearized variable
 #'
-#' @param exprlist what does exprlist need to be?
-#' @param infunlist what does infunlist need to be?
+#' @details the call must use function the \code{deriv} knows how to differentiate
 #'
-#' @details put details here
-#'
-#' @return what does this function return
+#' @return a list with two components: values - the estimate value and lin - the linearized variable
 #'
 #' @author Djalma Pessoa and Anthony Damico
 #'
-#' @seealso do you need a seealso block?
+#' @seealso \code{svysqr}
 #'
-#' @references do you need a references block?
-#' 
-#' @keywords keywords here
+#' @references Guillaume Osier (2009). Variance estimation for complex indicators
+#' of poverty and inequality. \emph{Journal of the European Survey Research
+#' Association}, Vol.3, No.3, pp. 167-195,
+#' ISSN 1864-3361, URL \url{http://ojs.ub.uni-konstanz.de/srm/article/view/369}.
+#'
 #'
 #' @examples
+#' library(survey)
+#' library(vardpoor)
+#' data(eusilc) ; names( eusilc ) <- tolower( names( eusilc ) )
 #'
-#' # usage example here
+#' # linearized design
+#' des_eusilc <- svydesign( ids = ~rb030 , strata = ~db040 ,  weights = ~rb050 , data = eusilc )
+#' w <- weights(des_eusilc)
+#' # ratio linearization
+#' T1 = list(value = sum(w*eusilc$eqincome) , lin = eusilc$eqincome )
+#' T2 = list(value = sum(w), lin = rep (1, nrow(eusilc)) )
+#' list_all <- list( T1 = T1, T2 = T2)
+#' lin_R = contrastinf (quote(T1/T2), list_all)
+#' # quintile share ratio linearization
+#' S20 <- svyisq(~ eqincome, design = des_eusilc, .20)
+#' S20_val <- coef (S20); attributes (S20_val) <- NULL
+#' S20_lin <- attr(S20 , "lin" )
+#' S80 <- svyisq(~ eqincome, design = des_eusilc, .80)
+#' S80_val <- coef (S80); attributes (S80_val) <- NULL
+#' S80_lin <- attr(S80 , "lin" )
+#' SU <- list (value =S80_val, lin = S80_lin )
+#' SI <- list (value =S20_val, lin = S20_lin )
+#' TOT <- list(value = sum( w * eusilc$eqincome) , lin = eusilc$eqincome )
+#' list_all <- list (TOT = TOT, SI = SI, SU = SU )
+#' lin_QSR <- contrastinf (quote((TOT-SU/SI)), list_all )
 #'
 #' @export
 contrastinf <- function(exprlist, infunlist) {
