9cc485040ebd690f7cc041e1072d954a47521ddf
Anthony Damico <ajdamico@gmail.com>
2016-06-23 14:45:57 -0400

    svylorenz and svyzenga are currently too slow for cran

--- R/svylorenz.R
+++ R/svylorenz.R
@@ -40,6 +40,7 @@
 #' @keywords survey
 #'
 #' @examples
+#' \dontrun{
 #' library(survey)
 #' library(vardpoor)
 #' data(eusilc) ; names( eusilc ) <- tolower( names( eusilc ) )
@@ -68,7 +69,6 @@
 #'
 #' # library(MonetDBLite) is only available on 64-bit machines,
 #' # so do not run this block of code in 32-bit R
-#' \dontrun{
 #' 
 #' # database-backed design
 #' library(MonetDBLite)
