30f1cfefa9c52e67e8adfbf810c66471929bccfb
Anthony Damico <ajdamico@gmail.com>
2016-07-19 00:23:46 -0400

    type out make.formula directly instead

--- R/survey_functions.R
+++ R/survey_functions.R
@@ -160,7 +160,7 @@ within_function_subset.DBIsvydesign <-
 		
 		vars_to_keep <- unique( c( all.vars(e) , names( x$variables ) ) )
 		
-		x$variables <- getvars(make.formula(vars_to_keep), x$db$connection, 
+		x$variables <- getvars(formula(paste("~", paste(vars_to_keep, collapse = "+"))), x$db$connection, 
 			x$db$tablename, updates = x$updates, subset = x$subset)
 			
 		r <- eval(e, x$variables, parent.frame())
